9a81df1e9aa3d4c77d76b850010da64159846f1b
Cory Metcalf <coco@Corys-MacBook-Pro.local>
2022-07-13 17:02:41 -0600

    adds assets folder

---
+++ Assets/notes.js
@@ -0,0 +1,12 @@
+// localStorage.setItem('key', "value/data");
+// localStorage.getItem('key')
+// // Localstorage can only save strings
+
+// let array = [1, 2, 3, 4];
+
+// localStorage.setItem('key2', JSON.stringify(array));
+// JSON.parse(localStorage.getItem('key2'))
+
+// "Hello, I am a a string"
+
+// let newArray = "['hi', 'hello', 'hey']"
